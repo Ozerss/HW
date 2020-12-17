53f03254067609775fc0b7346c39526583d108d7
Alexander <botvaozerss@gmail.com>
2020-12-17 03:31:47 +0300

    Initial commit

---
+++ 1.py
@@ -0,0 +1,10 @@
+var1 = 94
+var2 = 58
+var3 = 42
+print(var3)
+print(var2)
+print(var1)
+firstName = input("Enter your name: ")
+lastName = input("Enter your surname: ")
+age = input("Enter your actually age: ")
+print(f"Your name is {firstName} {lastName}, your age is {age}")
