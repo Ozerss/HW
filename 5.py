a7f36fa06983d179e1ae0062d6d3e7a183c06a69
Alexander <botvaozerss@gmail.com>
2020-12-17 08:16:41 +0300

    дз5

--- 5.py
+++ 5.py
@@ -1,9 +1,12 @@
 profit = int(input("Введите сумму выручки вашей компании: "))
 costs = int(input("Введите сумму издержек вашей компании: "))
 rent = profit - costs
-while profit > costs:
+rent_2 = float(profit / costs)
+print(f"Ваша рентабельность составляет {rent_2}")
+if profit == costs:
+    print("Ваша фирма выходит в ноль")
+if profit > costs:
     workers_count = int(input("Назовите число сотрудников вашей компании: "))
     print(f"На одного сотрудника вашей компании приходится {rent / workers_count} прибыли")
-    break
-else:
+if profit < costs:
     print(f'Ваша фирма имеет отрицательный финансовый результат (издержки больше выручки)')
